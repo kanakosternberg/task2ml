78c927bc9878b7a73dc820d7b9d21053a4cf3246
kanakosternberg <61282230+kanakosternberg@users.noreply.github.com>
2020-05-04 15:03:06 +0200

    Add files via upload

--- transf_don.py
+++ transf_don.py
@@ -11,11 +11,9 @@ import numpy as np
 
 df = pd.read_csv("C:/Users/ksdiv/Documents/ETHZ/mast_sem_II/machine learning/task2/train_features_cleaned.csv")
 
-
 a= df.isna().sum()
 b=len(df)
 c=a/b
-
 
 # decide de remplir avec knn seulement les valeurs qui sont en dessous de 40% inconnues
 
@@ -24,4 +22,16 @@ val_knn=c [(c < 0.4) & (c > 0.1)] #impression que manque que tres peu de valeurs
 val_sshgb=df[np.isnan(df['Hgb'])]
 x_hgb=df[~np.isnan(df['Hgb'])]
 xy_hgb=x_hgb.loc[:,'Hgb']
-xx_hgb=x_hgb.loc[:,~'Hgb']
+xx_hgb=x_hgb.loc[:,x_hgb.columns != 'Hgb']
+
+"""
+from sklearn.neighbors import KNeighborsRegressor
+neigh = KNeighborsRegressor(n_neighbors=2)
+neigh.fit(xx_hgb, xy_hgb)
+a= neigh.predict(val_sshgb)
+"""
+
+
+from sklearn.impute import KNNImputer
+imputer = KNNImputer(n_neighbors=2, weights="uniform")
+imputer.fit_transform(df)
